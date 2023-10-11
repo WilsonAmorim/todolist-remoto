fea89392e95af745351e2391586c42ba96eb0ece
WilsonAmorim <wilamsa@gmail.com>
2023-10-11 10:25:22 -0300

    commit inicial

---
+++ src/main/java/br/com/amorimlopes/tolist/TolistApplication.java
@@ -0,0 +1,13 @@
+package br.com.amorimlopes.tolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TolistApplication.class, args);
+	}
+
+}
